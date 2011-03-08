499e0ffb2c7834ad315a0bc2f2d18a5d7c174d19
wilkie <wilkie05@gmail.com>
2011-03-08 16:56:36 -0500

    Added Logo module to print the logo.

--- test.rb
+++ test.rb
@@ -1,3 +1,10 @@
 require_relative 'lib/gruesome/z/machine'
+require_relative 'lib/gruesome/logo'
+
+Gruesome::Logo.print
+
+puts
+puts "--------------------------------------------------------------------------------"
+puts
 
 m = Gruesome::Z::Machine.new('test/zork1.z3')
